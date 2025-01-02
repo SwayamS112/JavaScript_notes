548a1dec5cba1253da0ab3977fe580b957901d38
Swayam Sood <148384397+SwayamS112@users.noreply.github.com>
2025-01-02 13:56:45 +0530

    LOOPS

---
+++ 04_iterations/01_loop.js
@@ -0,0 +1,18 @@
+let index = 0
+while (index <=10) {
+  console.log(`Value of index is ${index}`);
+  index += 2
+}
+
+let hero = ["flash","ironman","hulk"]
+let arr2 = 0;
+while (arr2 < hero.length) {
+  console.log(`value is ${hero[arr2]}`);
+ arr2 += 1
+}
+
+let score =1
+do{
+console.log(`score is ${score}`);
+score++
+}while (score<=10);
