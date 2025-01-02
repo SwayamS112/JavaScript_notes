7d59083d56ce549b5b54b9d8099dbc5cf0b580ba
Swayam Sood <148384397+SwayamS112@users.noreply.github.com>
2025-01-02 14:30:04 +0530

    high loops

---
+++ 04_iterations/02_high_level_loops.js
@@ -0,0 +1,32 @@
+// const arr = [1,2,3,4,5,6]
+// for(const num of arr){
+// console.log(num);
+// }
+
+// const greatings = "Hello javascript"
+// for(const great of greatings){
+//     console.log(great);
+// }
+
+//maps ->
+const map = new Map()
+//       |->key  |-> value 
+map.set('IN', "India")
+map.set("USA", "UNITED STATES OF AMERICA")
+map.set("FR", "FRANCE")
+map.set('IN', "India") // DOES ADD SECOND TIME SAME 
+console.log(map);
+
+for(const [key,value] of map){
+    console.log(key, ": ",value);   
+}
+
+const coding =["js","ruby","html","c++","c","C#"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+coding.forEach((item)=>{
+console.log(item);
+})
